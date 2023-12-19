869e9dc0448db4faca82f45ade110b7a81121918
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2023-12-19 14:43:00 +1100

    added stuff

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    apps : [{
+        name: 'wattlefoxxocom',
+        script: '/var/www/wattlefoxxocom/current/index.js',
+        cwd: '/var/www/wattlefoxxocom/current',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '512M',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+    deploy : {
+        wattlefoxxocom : {
+            key  : '/root/.ssh/id_ed25519.pub',
+            user : 'root',
+            host : '192.168.50.102',
+            ref  : 'origin/main',
+            repo : 'git@github.com:WattleFoxxo/WebsiteMirror.git',
+            fetch : 'all',
+            path : '/var/www/wattlefoxxocom',
+            'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+        }
+    }
+  };
